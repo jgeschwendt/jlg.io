1ef14102f6632b81a2e16679a22ec4c1650a25de
jlg <joshua@geschwendt.com>
2018-08-15 23:20:07 -0400

    get a simple spa running

---
+++ Makefile
@@ -0,0 +1,65 @@
+# Load the environment variables
+ifdef ENV
+export ENV_FILE = .env.$(ENV)
+else
+export ENV_FILE = .env
+endif
+
+# Include the envionment variables in this Makefile
+include $(ENV_FILE)
+
+# Export these variables for docker-compose usage
+export CONTAINER_NAME = jlg.io/devbox
+export NODE_CONTAINER = \
+	--interactive \
+	--rm \
+	--tty \
+	--volume $(shell pwd):/var/task \
+	--workdir /var/task \
+	$(CONTAINER_NAME)
+
+build:
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) yarn run build
+
+deploy:
+	@make stop-docker
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) yarn run deploy
+
+check:
+	@make tsc
+	@make tslint
+
+dev:
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) /bin/bash
+
+devbox:
+	@docker build --no-cache --tag $(CONTAINER_NAME) .
+
+install:
+	@docker run $(NODE_CONTAINER) yarn install
+
+profile:
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) yarn run profile
+
+start:
+	@docker run --env-file $(ENV_FILE) --publish 3000:3000 $(NODE_CONTAINER) yarn run start
+
+start-hard:
+	rm -rf node_modules
+	rm -f yarn.lock
+	make devbox
+	make install
+	make start
+
+stop-docker:
+	@docker ps -aq | xargs docker stop
+	@docker ps -aq | xargs docker rm
+
+tsc:
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) yarn run tsc
+
+tslint:
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) yarn run tslint
+
+tslint-fix:
+	@docker run --env-file $(ENV_FILE) $(NODE_CONTAINER) yarn run tslint-fix
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'eval-source-map',
+  target: 'web',
+  entry: {
+    main: [
+      path.resolve(process.cwd(), 'src/index.ts')
+    ],
+  },
+  externals: [],
+  optimization: {
+    // https://webpack.js.org/plugins/split-chunks-plugin/#split-chunks-example-2
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'all',
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/
+        }
+      }
+    }
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(process.cwd(), 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        test: /\.(ts|tsx)$/,
+        use: [
+          { loader: require.resolve('tslint-loader') }
+        ]
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.(ts|tsx)$/,
+        use: [
+          { loader: require.resolve('babel-loader') },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin([
+      'NODE_ENV',
+      'STAGE'
+    ].reduce((obj, key) => ({ ...obj, [`process.env.${key}`]: JSON.stringify(process.env[key]) }), {}), {
+      'process.env.PACKAGE_VERSION': require('./package.json').version
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new CircularDependencyPlugin({ exclude: /node_modules/, failOnError: true }),
+    new HtmlWebpackPlugin({ filename: 'index.html', template: 'src/index.html' })
+  ],
+  resolve: {
+    extensions: [ '.wasm', '.mjs', '.js', '.json', '.jsx', '.ts', '.tsx' ],
+    modules: [ path.resolve(process.cwd(), 'node_modules'), 'node_modules', 'src' ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    host: process.env.HOST || '0.0.0.0',
+    port: 3000,
+    publicPath: '/'
+  },
+};
