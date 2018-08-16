de407f3fbe3abacd94ffe089af11520d195312c1
jgeschwendt <joshua@geschwendt.com>
2018-08-15 23:43:31 -0400

    add logo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,12 +13,22 @@ const App = styled.div`
 `
 
 const HelloWorld = styled.div`
-  padding: 2rem;
-  border: 1px solid #18453b;
+  border: .5rem solid #fff;
 `
+
+const SVG = styled.svg`
+  height: 300px;
+  width: 300px;
+`
+
+const jlgPath = 'M149,25 L149,60 L114,25 L149,25 Z M96,25 L96,131 C67,131 43,107 43,78 C43,48 66,25 96,25 Z M114,131 L114,78 L149,78 L149,131 C149,160 125,184 96,184 L96,149 C106,149 114,141 114,131 Z'
 
 export default () => (
   <App>
-    <HelloWorld>hello, world</HelloWorld>
+    <HelloWorld>
+      <SVG viewBox='0 0 192 192'>
+        <path d={jlgPath} fill='#fff' />
+      </SVG>
+    </HelloWorld>
   </App>
 )
