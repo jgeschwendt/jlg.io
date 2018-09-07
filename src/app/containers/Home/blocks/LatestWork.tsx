51b195950bd346117c43b8a2c2cb88b90676b68d
jlg <joshua@geschwendt.com>
2018-09-07 02:00:14 -0400

    add latest works section

--- src/app/containers/Home/blocks/LatestWork.tsx
+++ src/app/containers/Home/blocks/LatestWork.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react'
 import styled from '../../../styled'
 
-const LatestWork = styled.div``
-const Link = styled.a``
+const LatestWork = styled.div`
 
-export default () => (
-  <LatestWork>
-    <Link href="https://www.hurleyfoundation.org" target="_blank">Hurley Foundation Website</Link>
-    <Link href="https://thinkblackinc.com" target="_blank">BLACK Company Website</Link>
-    <Link href="https://imcs.solutions" target="_blank">IMCS Company Website</Link>
+`
+
+const Links = styled.a`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const Link = styled.a`
+  color: white;
+  margin: .5rem;
+  text-decoration: none;
+`
+
+const Statement = styled.p`
+  color: white;
+  text-transform: uppercase;
+  text-align: center;
+`
+
+export default ({ className = null }) => (
+  <LatestWork className={className}>
+    <Statement>Checkout some of my latest public works</Statement>
+    <Links>
+      <Link href="https://www.hurleyfoundation.org" target="_blank">https://www.hurleyfoundation.org</Link>
+      <Link href="https://thinkblackinc.com" target="_blank">https://thinkblackinc.com</Link>
+      <Link href="https://imcs.solutions" target="_blank">https://imcs.solutions</Link>
+    </Links>
   </LatestWork>
 )
