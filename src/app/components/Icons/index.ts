ba7851532d103280cde5d75474cdd6037200f205
Joshua L Geschwendt <joshua@geschwendt.com>
2020-02-29 13:58:07 -0500

    Add elm ts system

--- src/app/components/Icons/index.ts
+++ src/app/components/Icons/index.ts
@@ -1,6 +1,1 @@
-// Credit: https://fontawesome.com/license
-export { Envelope } from './Envelope';
-export { GitHub } from './GitHub';
-export { LinkedIn } from './LinkedIn';
-
 export { Entypo } from './Entypo';
