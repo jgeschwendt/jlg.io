f74ad4cce56da0663a35244a3687f9d3e072e05b
jgeschwendt <joshua@geschwendt.com>
2022-02-05 08:05:28 -0500

    use self-hosted font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const Lato = {
+  Light: 'fonts/Lato/Lato-Light.woff2',
+};
+
+const fontFace = `
+@font-face {
+  font-family: 'Lato';
+  src: url('${Lato.Light}') format('woff2');
+}
+`
+  .replace(/\n/g, '')
+  .trim();
+
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swaps"
-          rel="stylesheet"
-        />
+        <link as="font" href={Lato.Light} rel="preload" type="font/woff2" />
+        <style type="text/css" dangerouslySetInnerHTML={{ __html: fontFace }} />
       </Head>
       <body>
         <Main />
