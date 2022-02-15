867b9d4627a45cce5d44b0f3011dab0543060ed4
jgeschwendt <joshua@geschwendt.com>
2022-02-14 21:07:13 -0500

    updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,37 +1,36 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-const Lato = {
-  Light: '/fonts/Lato/Lato-Light.woff2',
-};
+const Lato = { Light: '/fonts/Lato/Lato-Light.woff2' };
 
-const fontFace = `
-@font-face {
-  font-family: 'Lato';
-  src: url('${Lato.Light}') format('woff2');
-}
-`;
+const Document = () => (
+  <Html>
+    <link
+      as="font"
+      crossOrigin=""
+      href={Lato.Light}
+      rel="preload"
+      type="font/woff2"
+    />
+    <Head />
+    <style
+      dangerouslySetInnerHTML={{
+        __html: `
+            @font-face {
+              font-display: block;
+              font-family: 'Lato';
+              src: url('${Lato.Light}') format('woff2');
+            }`
+          .replace(/\n/g, '')
+          .replace(/\s+/g, '')
+          .trim(),
+      }}
+      type="text/css"
+    />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <link
-        as="font"
-        crossOrigin=""
-        href={Lato.Light}
-        rel="preload"
-        type="font/woff2"
-      />
-      <style
-        type="text/css"
-        dangerouslySetInnerHTML={{
-          __html: fontFace.replace(/\n/g, '').replace(/\s+/g, '').trim(),
-        }}
-      />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+export default Document;
