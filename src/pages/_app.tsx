4f63ce6da87e1d341f1c01e03aa4ebcd39253c4a
jgeschwendt <joshua@geschwendt.com>
2022-04-28 12:03:59 -0400

    add Font component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,8 @@
 import 'modern-normalize/modern-normalize.css';
-import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
-
-const Lato = { Light: '/fonts/Lato/Lato-Light.woff2' };
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <link
-        as="font"
-        crossOrigin=""
-        href={Lato.Light}
-        rel="preload"
-        type="font/woff2"
-      />
-    </Head>
-    <Global
-      styles={{
-        '@font-face': {
-          fontDisplay: 'block',
-          fontFamily: 'Lato',
-          src: `url('${Lato.Light}') format('woff2')`,
-        },
-        'body': {
-          fontFamily: 'Lato, sans-serif',
-        },
-      }}
-    />
-    <Component {...pageProps} />
-  </>
+  <Component {...pageProps} />
 );
 
 export default App;
