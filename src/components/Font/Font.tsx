cc3d5d2bee8ac96c2412966f7469d3ac63c5955c
jgeschwendt <joshua@geschwendt.com>
2022-04-28 12:03:59 -0400

    add Font component

---
+++ src/components/Font/Font.tsx
@@ -0,0 +1,53 @@
+import { Global } from '@emotion/react';
+import Head from 'next/head';
+
+const Lato = {
+  medium: [500, '/fonts/Lato/Lato-Medium.woff2'],
+  light: [300, '/fonts/Lato/Lato-Light.woff2'],
+} as const;
+
+const FontLato = ({
+  fontDisplay,
+  weights,
+}: {
+  fontDisplay?: string;
+  weights: (keyof typeof Lato)[];
+}) => (
+  <>
+    <Head>
+      {weights.map((weight) => (
+        <link
+          as="font"
+          crossOrigin=""
+          href={Lato[weight][1]}
+          key={`font-lato-${weight}`}
+          rel="preload"
+          type="font/woff2"
+        />
+      ))}
+    </Head>
+    <Global
+      styles={{
+        ...weights.map((weight) => ({
+          '@font-face': {
+            fontDisplay: fontDisplay ?? 'swap',
+            fontFamily: 'Lato',
+            fontWeight: Lato[weight][0],
+            src: `url('${Lato[weight][1]}') format('woff2')`,
+          },
+        })),
+        body: {
+          fontFamily: 'Lato, sans-serif',
+        },
+      }}
+    />
+  </>
+);
+
+export const Font = ({
+  lato,
+  ...props
+}: {
+  fontDisplay?: 'block' | 'swap';
+  lato?: (keyof typeof Lato)[];
+}) => <>{lato && <FontLato weights={lato} {...props} />}</>;
