27b0b32a3136bf66faf9e191048c3d57ad0dbaf5
jgeschwendt <joshua@geschwendt.com>
2022-08-21 09:23:20 -0400

    updates

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -3,4 +3,9 @@ import File from '@fortawesome/fontawesome-free/svgs/solid/file.svg?svgr';
 import GitHub from '@fortawesome/fontawesome-free/svgs/brands/github.svg?svgr';
 import LinkedInIn from '@fortawesome/fontawesome-free/svgs/brands/linkedin-in.svg?svgr';
 
-export const Icon = { Envelope, File, GitHub, LinkedInIn };
+export const Icon = {
+  Envelope,
+  File,
+  GitHub,
+  LinkedInIn,
+};
