7b9253e2e4fd18275bc5a03b4dfec9cf952c5dce
jgeschwendt <joshua@geschwendt.com>
2022-08-21 10:30:05 -0400

    revert emotion cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import 'modern-normalize/modern-normalize.css';
-import createCache from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
-const cache = createCache({ key: 'c', stylisPlugins: [] });
-
 const App = ({ Component, pageProps }: AppProps) => (
-  <CacheProvider value={cache}>
-    <Component {...pageProps} />
-  </CacheProvider>
+  <Component {...pageProps} />
 );
 
 export default App;
