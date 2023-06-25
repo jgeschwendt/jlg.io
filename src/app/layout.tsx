9e435c544ea2c23424d7f7b9c16ea86824fd3b21
jgeschwendt <joshua@geschwendt.com>
2023-06-25 10:02:09 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
 import { Lato } from 'next/font/google';
 import { statement } from './statement';
 
@@ -20,10 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html className={lato.variable} lang="en-US">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body className="bg-[#1a1a1a] font-light text-white">{children}</body>
+      <body className="bg-[#1a1a1a] font-light text-white">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
