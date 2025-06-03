8fe8c38c7806d1547587ce6d846627266239647b
jgeschwendt <joshua@geschwendt.com>
2025-06-03 00:00:22 -0400

    task(//): setup refinement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/app/global.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import type { Metadata } from 'next';
 import { headers } from 'next/headers';
 import type { JSX, PropsWithChildren } from 'react';
@@ -22,9 +23,9 @@ export default function RootLayout({
   children,
 }: PropsWithChildren): JSX.Element {
   return (
-    <html lang="en-US">
+    <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang="en-US">
       <body
-        className={`${GeistSans.variable} bg-[oklch(.1_0_0)] font-[family-name:var(--font-geist-sans)] font-extralight text-white`}
+        className="bg-[oklch(.1_0_0)] font-extralight text-white"
       >
         {children}
         <Analytics />
